112436bc69022e81e3595e9d8fd5f9ea7b37852f
charleswain <chen2en@aliyun.com>
2018-03-25 21:21:52 +0800

    * 调整版本

--- HelloSpringMVC/src/main/java/com/test/HelloSpringController.java
+++ HelloSpringMVC/src/main/java/com/test/HelloSpringController.java
@@ -5,22 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.test.dao.BookDao;
+import com.test.mapper.BookMapper;
 import com.test.dao.Book;
  
 @Controller
 public class HelloSpringController {
-	private BookDao bookDao;
-    String message = "Welcome to Spring MVC!";
+	private BookMapper bookDao;
+    String message = "Welcome to Spring MVC!!!";
  
     @RequestMapping("/hello")
     public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "Spring") String name) {
  
-    	long bookId =1000;
-    	Book book = bookDao.queryById(bookId);
-
-    	String bookname = book.getName();
+    	long bookId = 1000;
+    	Book book = bookDao.selectByPrimaryKey(bookId);
     	
+    	
+    	String bookname = book.getName();
+    	//System.out.print(book);
         ModelAndView mv = new ModelAndView("hellospring");//指定视图
         //向视图中添加所要展示或使用的内容，将在页面中使用
         mv.addObject("message", message);
